9280736e42ac7b2d34c376d49fd9fc458a9c7fae
Halenina <galenina20032101@gmail.com>
2024-03-10 03:18:10 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,12 +5,12 @@ const BASE_URL = 'https://pixabay.com/api/';
 
 
 export async function searchImages(searchTerm, page, perPage = 15) {
-	const API_KEY = '42541165-a3d46185342d32ff55345145c';
+    const API_KEY = '42541165-a3d46185342d32ff55345145c';
 
-	try {
-		const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(searchTerm)}&page=${page}&per_page=${perPage}`);
-		return response.data.hits;
-	} catch (error) {
-		throw new Error('Failed to fetch images from Pixabay API');
-	}
+    try {
+        const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(searchTerm)}&page=${page}&per_page=${perPage}`);
+        return response.data.hits;
+    } catch (error) {
+        throw new Error('Failed to fetch images from Pixabay API');
+    }
 }
